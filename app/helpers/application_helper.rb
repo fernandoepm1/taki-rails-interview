3e08f83ff276a1ee5e383bf2ce6652a73d67058a
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-19 12:56:12 -0300

    Add helper method to show validation errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def error_for(model, *attributes)
+    return if model.nil?
+
+    mensagem = []
+    attributes.each do |attr|
+      model.errors[attr].each { |error| mensagem << error if mensagem.exclude?(error) }
+    end
+
+    content_tag :span, class: 'error_explanation' do
+      mensagem.join(', ')
+    end
+  end
 end
