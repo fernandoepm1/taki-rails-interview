771b447fc5039812997594dcd46185516f1baaaa
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-19 12:52:09 -0300

    Complete user registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,17 @@ class SessionsController < ApplicationController
   # skip authentication for methods
   skip_before_action :authenticate_user, :only => [:new, :create]
 
-  def new
-
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to user
     else
       flash.now[:alert] = 'Invalid email/password combination'
-      render 'new'
+      render :new
     end
   end
 
