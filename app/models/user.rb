0ce3aa9c5f3a1ad8432a5e8a5a1b861f49795a73
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-06-19 17:12:23 -0300

    Add rspec main files and configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ActiveRecord::Base
 
   has_one :business
 
-  validates :first_name,  presence: true
-  validates :last_name,  presence: true
-  validates :email, presence: true
+  validates :first_name, :last_name, :email,  presence: true
 end
